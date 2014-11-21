dbcf27ff1a16ba5a8f9481ad65c0497b5924fbbc
Mark Bucciarelli <mkbucc@gmail.com>
2014-11-20 21:26:23 -0500

    Use HTML email so I can read it on my phone.

--- ai.sh
+++ ai.sh
@@ -4,10 +4,13 @@
 # Author: mkbucc@gmailcom
 #
 #	Usage example (in crontab):
-#		./ai.sh 1>&2
+#		./ai.sh
 #
 
 HOST=vps44276563
+
+# HTML mail looks sooo much better on my phone.
+OUTFN=/tmp/$(basename $0)-mail.$$.tmp
 
 # So we can put comments in the stop lists
 TMP1=/tmp/$(basename $0)-files.$$.tmp
@@ -15,15 +18,52 @@ TMP2=/tmp/$(basename $0)-rules.$$.tmp
 grep -v '\(^#\|^[:blank:]*$\)' /root/bin/stopfiles > $TMP1
 grep -v '\(^#\|^[:blank:]*$\)' /root/bin/stoplist > $TMP2
 
-find /var/log -type f | grep -v -f $TMP1 | while read fn; do
+
+[ "x$MAILTO" != "x" ] && TO=$MAILTO || TO=$USER
+
+# doctype and xmlns guidance from http://htmlemailboilerplate.com/.
+
+cat > $OUTFN << EOF
+To: $TO
+Subject: Log Bot yammered on $(date +%F)
+Content-Type: text/html
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Log Bot yammered on $(date +%F)</title>
+</head>
+<body style="margin: 5%; padding: 0; font-family: Courier, monospace">
+EOF
+
+# Piping into while creates a subshell,
+# which can't update variables.
+# So use line count change to figure out
+# if we need to send email.
+N0=$(cat $OUTFN | wc -l)
+find /var/log -type f | grep -v -f $TMP1 |  while read fn; do
     if grep -v -f $TMP2 $fn > /dev/null ; then
-        echo ""
-        echo "---------------------------------------------"
-        echo $fn
-        echo "---------------------------------------------"
-    	grep -n -v -f $TMP2 $fn
+        echo "<h2>$fn</h2>" >> $OUTFN
+        echo "<pre>" >> $OUTFN
+
+	# Add paragraph breaks
+	# so if phone wraps log lines
+	# we can still distinguish.
+	grep -n -v -f $TMP2 $fn |sed 's/$/<p>/' >> $OUTFN
+
+        echo "</pre>" >> $OUTFN
     fi
 done
+N1=$(cat $OUTFN | wc -l)
 
-rm -f $TMP1
-rm -f $TMP2
+cat >> $OUTFN << EOF
+</body>
+</html>
+EOF
+if [ $N1 -gt $N0 ] ; then
+	sendmail -t < $OUTFN
+fi
+
+rm -f $TMP1 $TMP2 $OUTFN
